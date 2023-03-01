70b9b86cd94873b0af57dc66237803b7c0eacdab
Lars Braitinger <Lars.Braitinger@netcetera.com>
2023-03-02 00:10:51 +0100

    Change light/dark theme toggle to a custom built switch

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,27 +1,59 @@
-import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { useColorMode } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
+
+const StyledSwitchBackground = styled('div')<{ theme: string }>`
+  display: inline-flex;
+  align-items: center;
+  border-radius: 1.5rem;
+  background-color: ${props =>
+          props.theme === 'light'
+                  ? 'var(--chakra-colors-orange-200)'
+                  : 'var(--chakra-colors-purple-500)'};
+`;
+
+const StyledSwitchButton = styled('button')<{ isActive: boolean }>`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props =>
+          props.isActive ? 'rgb(255, 255, 255)' : 'transparent'};
+  color: ${props => (props.isActive ? 'rgb(0, 0, 0)' : 'inherit')};
+`;
 
 export const ThemeToggleButton = () => {
   const { toggleColorMode } = useColorMode();
+  const [isLightMode, setIsLightMode] = useState<boolean>(() => {
+    const storedColorMode = typeof localStorage !== 'undefined' && localStorage.getItem('chakra-ui-color-mode');
+    return storedColorMode ? storedColorMode === 'light' : window.matchMedia('(prefers-color-scheme: light)').matches;
+  });
+
+  const toggleTheme = () => {
+    setIsLightMode(prevMode => !prevMode);
+    toggleColorMode();
+  };
 
   return (
-    <AnimatePresence mode={'wait'} initial={false}>
-      <motion.div
-        style={{ display: 'inline-block' }}
-        key={useColorModeValue('light', 'dark')}
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: 20, opacity: 0 }}
-        transition={{ duration: 0.2 }}
+    <StyledSwitchBackground theme={isLightMode ? 'light' : 'dark'}>
+      <StyledSwitchButton
+        onClick={toggleTheme}
+        aria-label="Toggle theme"
+        isActive={isLightMode}
       >
-        <IconButton
-          aria-label="Toggle theme"
-          colorScheme={useColorModeValue('purple', 'orange')}
-          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-          onClick={toggleColorMode}
-        ></IconButton>
-      </motion.div>
-    </AnimatePresence>
+        <SunIcon />
+      </StyledSwitchButton>
+      <StyledSwitchButton
+        onClick={toggleTheme}
+        aria-label="Toggle theme"
+        isActive={!isLightMode}
+      >
+        <MoonIcon />
+      </StyledSwitchButton>
+    </StyledSwitchBackground>
   );
 };
