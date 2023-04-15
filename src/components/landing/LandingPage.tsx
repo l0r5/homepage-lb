f2c07472daf85e6c0c8520546c7d4195cdd2dd71
Lars Braitinger <Lars.Braitinger@netcetera.com>
2023-04-15 12:28:02 +0200

    Improve landing page layout and replace picture

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -12,6 +12,14 @@ import SurfKitty from '../surfkitty/SurfKitty';
 export const LandingPage = (): ReactElement => {
   return (
     <>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as={'h2'} variant={'page-title'}>
+            Lars Braitinger
+          </Heading>
+          <p>Software Engineer</p>
+        </Box>
+      </Box>
       <SurfKitty />
       <Container maxW={'container.lg'}>
         <Box borderRadius={'lg'} p={3} mb={6} alignContent={'center'}
@@ -19,62 +27,66 @@ export const LandingPage = (): ReactElement => {
              css={{ backdropFilter: 'blur(10px)' }}>
           Hi, I'm a Full-Stack Developer based in Switzerland who loves to connect & create.
         </Box>
-        <Box display={{ md: 'flex' }}>
-          <Box flexGrow={1}>
-            <Heading as={'h2'} variant={'page-title'}>
-              Lars Braitinger
-            </Heading>
-            <p>Software Engineer</p>
-          </Box>
-          <Box
-            flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
-            textAlign="center"
-          >
-            <Box
-              borderColor="whiteAlpha.800"
-              borderWidth={2}
-              borderStyle="solid"
-              w="100px"
-              h="100px"
-              display="inline-block"
-              borderRadius="full"
-              overflow="hidden"
-            >
-              <ProfileImage
-                src="/images/lars.jpeg"
-                alt="Profile image"
-                borderRadius="full"
-                width="100"
-                height="100"
-              />
-            </Box>
-          </Box>
-        </Box>
         <Section delay={0.1}>
           <Heading as={'h3'} variant={'section-title'}>
             Intro
           </Heading>
-          <Paragraph>As a passionate and dedicated software engineer, I bring industry experience in designing,
-            developing, and maintaining both web and mobile applications using a diverse range of tech stacks. I am
-            proficient in understanding clients' needs and delivering projects that meet their requirements. With a
-            strong
-            academic background and hands-on experience in transforming software-related business ideas into reality, I
-            am
-            committed to collaborating with teams to deliver high-quality products on time.</Paragraph>
-          <br />
-          <Paragraph>
-            Throughout my journey, I have honed my skills in various aspects of software engineering and am always eager
-            to take on challenging roles that allow me to apply and grow my expertise. I invite you to explore my
-            portfolio to discover the exciting projects I've had the pleasure of working on, and learn more about how I
-            can help bring your vision to life.
-          </Paragraph>
-          <Box textAlign={'center'} my={4} mt={10} mb={20}>
-            <Button as={Link} href={'/works'} rightIcon={<ChevronRightIcon />} colorScheme={'teal'}>
-              My portfolio
-            </Button>
+          <Box
+            display={{ base: 'block', md: 'flex' }}
+            mt={{ base: 4, md: 0 }}
+          >
+            <Box
+              flexShrink={0}
+              order={{ base: 0, md: 1 }}
+              mt={{ base: 4, md: 0 }}
+              ml={{ md: 6 }}
+              textAlign="center"
+            >
+              <Box
+                borderColor="whiteAlpha.800"
+                borderWidth={2}
+                borderStyle="solid"
+                w="100px"
+                h="100px"
+                display="inline-block"
+                borderRadius="full"
+                overflow="hidden"
+              >
+                <ProfileImage
+                  src="/images/lars.jpeg"
+                  alt="Profile image"
+                  borderRadius="full"
+                  width="100"
+                  height="100"
+                />
+              </Box>
+            </Box>
+            <Box
+              flex="1"
+              order={{ base: 1, md: 0 }}
+              pr={{ md: 6 }}
+            >
+              <Paragraph>As a passionate and dedicated software engineer, I bring industry experience in designing,
+                developing, and maintaining both web and mobile applications using a diverse range of tech stacks. I am
+                proficient in understanding clients' needs and delivering projects that meet their requirements. With a
+                strong
+                academic background and hands-on experience in transforming software-related business ideas into reality, I
+                am
+                committed to collaborating with teams to deliver high-quality products on time.</Paragraph>
+              <br />
+              <Paragraph>
+                Throughout my journey, I have honed my skills in various aspects of software engineering and am always eager
+                to take on challenging roles that allow me to apply and grow my expertise. I invite you to explore my
+                portfolio to discover the exciting projects I've had the pleasure of working on, and learn more about how I
+                can help bring your vision to life.
+              </Paragraph>
+            </Box>
           </Box>
+          {/*<Box textAlign={'center'} my={4} mt={10} mb={20}>*/}
+          {/*  <Button as={Link} href={'/works'} rightIcon={<ChevronRightIcon />} colorScheme={'teal'}>*/}
+          {/*    My portfolio*/}
+          {/*  </Button>*/}
+          {/*</Box>*/}
         </Section>
         <Section delay={0.2}>
           <Heading as={'h3'} variant={'section-title'}>
